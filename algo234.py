f37bac6ff8b611c7cbb41d05484d7ebdf041fe88
vikashanumegowda <hanmego@usc.edu>
2017-04-20 22:34:50 -0700

    New Problems from Leetcode

--- algo234.py
+++ algo234.py
@@ -1,3 +1,9 @@
+'''
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+'''
 class Solution(object):
     def isPalindrome(self, head):
         """
