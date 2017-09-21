fb3e10a66e4b4d755ed35f8da283a75aa756ff3b
Vikas Hanumegowda <vikas.hanumegowda@gmail.com>
2017-09-21 12:40:38 -0700

    Create Leetcode26.py
    New algo from leetcode

---
+++ Leetcode26.py
@@ -0,0 +1,30 @@
+'''
+   Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length. 
+
+'''
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = len(nums)
+        if l==0 or l==1:
+            return l        
+        x=0
+        l = len(nums)-1
+        while x < l:
+            if nums[x] == nums[x+1]:
+                nums.pop(x+1)
+                l = len(nums)-1
+            else:
+                x+=1
+        l = len(nums)
+        return l
