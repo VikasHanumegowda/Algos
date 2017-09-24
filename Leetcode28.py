e48adaf1ffe90138ce2582f38f91bc82668318d0
vikashanumegowda <hanumego@usc.edu>
2017-09-23 19:48:36 -0700

    spacing in files

--- Leetcode28.py
+++ Leetcode28.py
@@ -3,6 +3,8 @@
 
 Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. 
 '''
+
+
 class Solution(object):
     def strStr(self, haystack, needle):
         """
@@ -12,18 +14,17 @@ class Solution(object):
         """
         l_h = len(haystack)
         l_n = len(needle)
-        
+
         if l_h == l_n == 0:
             return 0
         list_h = [x for x in haystack]
         list_n = [x for x in needle]
-        
-        for x in range(l_h-l_n+1):
-            count =0
+
+        for x in range(l_h - l_n + 1):
+            count = 0
             for y in range(l_n):
-                if list_h[x+y]==list_n[y]:
-                    count+=1
+                if list_h[x + y] == list_n[y]:
+                    count += 1
             if count == l_n:
                 return x
         return -1
-            
